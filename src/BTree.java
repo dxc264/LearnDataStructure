a164b57c0ab2418f9ea76d9106aab0bd2ec89d25
tle264 <Thuy_Le_TM@mfcgd.com>
2020-02-12 16:12:23 +0700

    refactor method name

--- src/BTree.java
+++ src/BTree.java
@@ -276,7 +276,7 @@ public class BTree<K extends Comparable<K>, V> {
         }
     }
 
-    private void rotateRight(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
+    private void rotateLeftTreeToRight(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
         Entry separatorEntry = parentNode.keyList[separatorIndex];
         for (int i = deficientNode.numberOfKeys; i > 0; i--) {
             deficientNode.keyList[i] = deficientNode.keyList[i-1];
@@ -291,7 +291,7 @@ public class BTree<K extends Comparable<K>, V> {
         parentNode.keyList[separatorIndex].next = deficientNode;
     }
 
-    private void rotateLeft(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
+    private void rotateRightTreeToLeft(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
         Entry separatorEntry = parentNode.keyList[separatorIndex];
         deficientNode.keyList[deficientNode.numberOfKeys] = new Entry(separatorEntry.key, separatorEntry.val);
         deficientNode.keyList[deficientNode.numberOfKeys].prev = siblingNode.keyList[siblingNode.numberOfKeys-1].next;
@@ -306,17 +306,17 @@ public class BTree<K extends Comparable<K>, V> {
         parentNode.keyList[separatorIndex].next = deficientNode;
     }
 
-    private Node mergeAllToLeft(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
+    private Node mergeWithLeftSibling(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
         deficientNode.keyList[deficientNode.numberOfKeys] = new Entry(parentNode.keyList[separatorIndex].key, parentNode.keyList[separatorIndex].val);
         deficientNode.numberOfKeys++;
         for (int i = 0; i < siblingNode.numberOfKeys; i++) {
             deficientNode.keyList[deficientNode.numberOfKeys] = new Entry(siblingNode.keyList[i].key, siblingNode.keyList[i].val);
             deficientNode.numberOfKeys++;// deficientNode is full
         }
-        return packAfterMergeChilAndDeleteParent(parentNode, separatorIndex, deficientNode);
+        return packNodeAfterDeleteEntry(parentNode, separatorIndex, deficientNode);
     }
 
-    private Node mergeAllToRight(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
+    private Node mergeWithRightSibling(Node parentNode, int separatorIndex, Node deficientNode, Node siblingNode) {
         Node newNode = new Node(siblingNode.numberOfKeys + 1 + deficientNode.numberOfKeys);
         int i = 0;
         for (i = 0; i < siblingNode.numberOfKeys; i++) {
@@ -326,10 +326,10 @@ public class BTree<K extends Comparable<K>, V> {
         for (int j = 0; j < deficientNode.numberOfKeys; j++) {
             newNode.keyList[++i] = deficientNode.keyList[j];
         }
-        return packAfterMergeChilAndDeleteParent(parentNode, separatorIndex, newNode);
+        return packNodeAfterDeleteEntry(parentNode, separatorIndex, newNode);
     }
 
-    private Node  packAfterMergeChilAndDeleteParent(Node parentNode, int separatorIndex, Node newNode) {
+    private Node packNodeAfterDeleteEntry(Node parentNode, int separatorIndex, Node newNode) {
         parentNode.keyList[separatorIndex - 1].next = newNode;
         if (parentNode.keyList[separatorIndex + 1] != null) {
             parentNode.keyList[separatorIndex + 1].prev = newNode;
@@ -347,17 +347,17 @@ public class BTree<K extends Comparable<K>, V> {
         Node newNode = null;
         if (parentNode.keyList[separatorIndex].prev.numberOfKeys < parentNode.keyList[separatorIndex].next.numberOfKeys) {
             if (parentNode.keyList[separatorIndex].next.numberOfKeys > MIN_KEYS_COUNT) {
-                rotateLeft(parentNode, separatorIndex, parentNode.keyList[separatorIndex].prev, parentNode.keyList[separatorIndex].next);
+                rotateRightTreeToLeft(parentNode, separatorIndex, parentNode.keyList[separatorIndex].prev, parentNode.keyList[separatorIndex].next);
                 return null;
             } else {
-                newNode = mergeAllToLeft(parentNode, separatorIndex, parentNode.keyList[separatorIndex].prev, parentNode.keyList[separatorIndex].next);
+                newNode = mergeWithLeftSibling(parentNode, separatorIndex, parentNode.keyList[separatorIndex].prev, parentNode.keyList[separatorIndex].next);
             }
         } else {
             if (parentNode.keyList[separatorIndex].prev.numberOfKeys > MIN_KEYS_COUNT) {
-                rotateRight(parentNode, separatorIndex, parentNode.keyList[separatorIndex].next, parentNode.keyList[separatorIndex].prev);
+                rotateLeftTreeToRight(parentNode, separatorIndex, parentNode.keyList[separatorIndex].next, parentNode.keyList[separatorIndex].prev);
                 return null;
             } else {
-                newNode = mergeAllToRight(parentNode, separatorIndex, parentNode.keyList[separatorIndex].prev, parentNode.keyList[separatorIndex].next);
+                newNode = mergeWithRightSibling(parentNode, separatorIndex, parentNode.keyList[separatorIndex].prev, parentNode.keyList[separatorIndex].next);
             }
         }
         if (parentNode.numberOfKeys < MIN_KEYS_COUNT) {
